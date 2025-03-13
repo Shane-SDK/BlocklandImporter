5e989ad1cc3d76de3d085ac8685466d8b5f1f5ff
ShaneSDK <totalminerexplorer@gmail.com>
2025-03-13 15:33:06 -0400

    Custom SaveData inspector

---
+++ Assets/BlocklandImporter/Code/Editor/Inspectors/SaveDataInspector.cs
@@ -0,0 +1,20 @@
+﻿using Blockland.Objects;
+using UnityEditor;
+using UnityEngine;
+
+namespace Blockland
+{
+    [CustomEditor(typeof(SaveData))]
+    public class SaveDataInspector : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            SaveData save = target as SaveData;
+
+            GUI.enabled = false;
+            EditorGUILayout.LabelField($"{save.bricks.Count} bricks");
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("bounds"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("colorMap"));
+        }
+    }
+}
